88e16cd186e2bc68fac07c9873ac092ff1f6f059
RAINNY4114 <152569703+RAINNY4114@users.noreply.github.com>
2025-06-17 16:51:08 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
+export ATHENA_HOST='wss://athena.konik.ai/'
+export API_HOST='https://api.konik.ai'
+export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
+export SKIP_FW_QUERY=1
 
 exec ./launch_chffrplus.sh
