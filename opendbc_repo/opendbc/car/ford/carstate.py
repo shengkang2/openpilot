14fe3a07c6342c81930446bafb1f51c19707bb68
RAINNY4114 <152569703+RAINNY4114@users.noreply.github.com>
2025-08-17 13:52:14 +0800

    Update carstate.py

--- opendbc_repo/opendbc/car/ford/carstate.py
+++ opendbc_repo/opendbc/car/ford/carstate.py
@@ -129,11 +129,11 @@ class CarState(CarStateBase, MadsCarState):
       if self.CP.flags & FordFlags.CANFD:
         gear = self.shifter_values.get(cp.vl["Gear_Shift_by_Wire_FD1"]["TrnRng_D_RqGsm"])
       elif self.CP.flags & FordFlags.ALT_STEER_ANGLE:
-           gear = self.shifter_values.get(cp.vl["TransGearData"]["GearLvrPos_D_Actl"])
-      else:
-        gear = self.shifter_values.get(cp.vl["PowertrainData_10"]["TrnRng_D_Rq"])
-
-      ret.gearShifter = self.parse_gear_shifter(gear)
+        if (cp.vl["TransGearData"]["GearLvrPos_D_Actl"] in (3, 4, 5)):
+          ret.gearShifter = GearShifter.drive
+        elif (cp.vl["TransGearData"]["GearLvrPos_D_Actl"] == 1):
+          ret.gearShifter = self.parse_gear_shifter(gear)
+          
     elif self.CP.transmissionType == TransmissionType.manual:
       ret.clutchPressed = cp.vl["Engine_Clutch_Data"]["CluPdlPos_Pc_Meas"] > 0
       if bool(cp.vl["BCM_Lamp_Stat_FD1"]["RvrseLghtOn_B_Stat"]):
